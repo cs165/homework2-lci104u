05b35ad81f0318afd34ad1ab490d457c4cb95c29
behere <linlin001@csie.io>
2019-04-28 21:41:48 +0800

    part2 evil complete

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -12,9 +12,18 @@ const MATCH_LIST = {
 
 function transformTextNodes(node) {
   // TODO(you): Implement this function! See HW spec for details.
+  if(node.nodeType == Node.TEXT_NODE) {
+    let str = node.textContent.split(/\s+/);
+    str = str.map ( chr => { return  MATCH_LIST.hasOwnProperty(chr)? MATCH_LIST[chr] : chr;  } );
+    node.textContent = str.join(' ');
+  }
+  for (const child of node.childNodes) {
+    if (child.nodeName !== 'SCRIPT' && child.nodeName !=='STYLE' ) transformTextNodes(child);
+  }
 }
 
 transformTextNodes(document.body);
 
 // Log statement to test that the extension loaded properly.
 console.log('Evil extension loaded!');
+console.log('Extension updated');
